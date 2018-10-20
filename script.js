f44218716c31d5642f6c73f0b824e0bd7e6f4499
markrmessmore <markrmessmore@hotmail.com>
2018-10-20 13:19:56 -0400

    Added a sort function and a few definitions

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
+//INITIAL FETCH OF THE DICTIONARY
 fetch('./dictionary.json').then(function(response) {
   // Error handling
   if (response.status !== 200) {
     console.log('Problem occurred fetching dictonary JSON.');
     return;
   }
-
   // Data handling and DOM manipulation
   response.json().then(function(data) {
-    data.forEach(function(item) {
-      entry = document.createElement('div');
-      word = document.createElement('h3');
-      definition = document.createElement('p');
-
-      word.textContent = item.word;
-      word.classList = 'word';
-
-      definition.classList = 'definition';
-      definition.innerHTML = item.definition;
-
-      entry.classList = 'entry';
-      entry.appendChild(word);
-      entry.appendChild(definition);
-
-      document.querySelector('#dictionary').appendChild(entry);
-    });
-    console.log('Dictionary loaded and rendered.');
+    //NOW WE SORT THE DATA BEFORE PUSHING IT
+    data.sort((definitionA, definitionB) => {
+      if (definitionA.word.toLowerCase() > definitionB.word.toLowerCase()) {
+        return 1
+      }
+      else {
+        return -1
+      }
+    })
+    pushDefinitions(data)
   });
 });
+
+//PUSH THE DEFINITIONS TO THE WEBSITE
+function pushDefinitions(data) {
+  data.forEach(function(item) {
+    entry = document.createElement('div');
+    word = document.createElement('h3');
+    definition = document.createElement('p');
+
+    word.textContent = item.word;
+    word.classList = 'word';
+
+    definition.classList = 'definition';
+    definition.innerHTML = item.definition;
+
+    entry.classList = 'entry';
+    entry.appendChild(word);
+    entry.appendChild(definition);
+
+    document.querySelector('#dictionary').appendChild(entry);
+  });
+  console.log('Dictionary loaded and rendered.');
+}
