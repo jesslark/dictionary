bd2a575d206b158a3f677a4ac3f8d2e4fc64aabc
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-10-20 14:13:23 +0800

    Add fetch for JSON reading and DOM rendering of dictionary JSON items

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert('this works')
+fetch('./dictionary.json').then(function(response) {
+  // Error handling
+  if (response.status !== 200) {
+    console.log('Problem occurred fetching dictonary JSON.');
+    return;
+  }
+
+  // Data handling and DOM manipulation
+  response.json().then(function(data) {
+    data.forEach(function(item) {
+      element = document.createElement('div');
+      word = document.createElement('h3');
+      definition = document.createElement('p');
+
+      word.textContent = item.word;
+      definition.textContent = item.definition;
+      element.appendChild(word);
+      element.appendChild(definition);
+
+      document.querySelector('.container').appendChild(element);
+    });
+    console.log('Dictionary loaded and rendered.');
+  });
+});
