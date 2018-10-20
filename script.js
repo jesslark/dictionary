5fcc14ee569ad788184a39e21a70f3008d9edf1d
StrongGood <27102345+StrongGood@users.noreply.github.com>
2018-10-20 12:47:44 -0700

    Added search functionality

--- script.js
+++ script.js
@@ -41,3 +41,50 @@ function pushDefinitions(data) {
   });
   console.log('Dictionary loaded and rendered.');
 }
+// https://stackoverflow.com/a/26494414
+function gid(a_id) {
+  return document.getElementById(a_id);
+} 
+function close_all() {
+
+  // applies the Array.prototype.forEach() method to the array-like nodeList
+  // returned by document.querySelectorAll() (the string passed to which finds all
+  // elements with a class that starts with ('^=') the string 'entry':
+  [].forEach.call(document.querySelectorAll('[class^=entry]'), function(div) {
+    // 'div' is the array element (the node) itself:
+    div.style.display = 'none';
+  });
+
+}
+
+
+function find_my_div() {
+  close_all();
+
+  // getting the trimmed lower-cased string from the input element, split
+  // on white-space characters to create an array:
+  var keywords = gid('edit_search').value.trim().toLowerCase().split(/\s+/),
+    // as above, selecting all elements whose id starts with the string 'entry':
+    haystack = document.querySelectorAll('[class^="entry"]'),
+    // working out whether text is accessed by node.textContent, or node.innerText:
+    textProp = 'textContent' in document.body ? 'textContent' : 'innerText',
+    // an initialised variable, for later:
+    userWords,
+    // filters the haystack (the divs whose id starts with 'user_'):
+    found = [].filter.call(haystack, function(user) {
+      // assigns the lower-cased string to the created-variable:
+      userWords = user[textProp].toLowerCase();
+      // returns those div elements whose text contains some of
+      // the words returned, earlier, as the keywords:
+      return keywords.some(function (word) {
+        return userWords.indexOf(word) > -1;
+      });
+    });
+
+  // iterates over the found elements, and shows them:
+  [].forEach.call(found, function(user) {
+    user.style.display = 'block';
+  });
+
+}
+
