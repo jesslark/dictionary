95d7929a7dc3fb99e2985e2ea64f1a679031235f
Jess Mear <jessmeardev@gmail.com>
2019-10-14 00:29:30 -0700

    Updates

--- precache-manifest.5b868143c3436f3aceea688fa4a65dc4.js
+++ precache-manifest.5b868143c3436f3aceea688fa4a65dc4.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ae07d0de16b890787ef024cb6a981aaa",
+    "revision": "6667b3664a549fa716a03b3af44e1d27",
     "url": "/dictionary/index.html"
   },
   {
-    "revision": "c534c95161b5946621b1",
-    "url": "/dictionary/static/css/main.05ecc33f.chunk.css"
+    "revision": "7b067d9934adafd1f536",
+    "url": "/dictionary/static/css/main.168006bd.chunk.css"
   },
   {
     "revision": "8568f50f7ceb80227548",
     "url": "/dictionary/static/js/2.d42b6f0b.chunk.js"
   },
   {
-    "revision": "c534c95161b5946621b1",
+    "revision": "7b067d9934adafd1f536",
     "url": "/dictionary/static/js/main.7b358422.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dictionary/precache-manifest.1cc67efb3a3a58a21d271af9a8df9cb8.js"
+  "/dictionary/precache-manifest.5b868143c3436f3aceea688fa4a65dc4.js"
 );
 
 self.addEventListener('message', (event) => {
