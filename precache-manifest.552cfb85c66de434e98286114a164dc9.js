30c1d5d2b6b51c324e2b5ced84770d8152594a8d
Jess Lark <jess.lark@cobalt.io>
2023-04-12 13:06:59 -0700

    Updates

--- precache-manifest.552cfb85c66de434e98286114a164dc9.js
+++ precache-manifest.552cfb85c66de434e98286114a164dc9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2bec122de2b200d0692b4f4f6622c6e8",
+    "revision": "4ac4a12eca49bcfade97c9e5bfd87787",
     "url": "/dictionary/index.html"
   },
   {
-    "revision": "7f913f7d80bac4f8fb22",
-    "url": "/dictionary/static/css/main.ee2f0e55.chunk.css"
+    "revision": "5dc6acc4edd0bd369aca",
+    "url": "/dictionary/static/css/main.1a59c986.chunk.css"
   },
   {
     "revision": "8568f50f7ceb80227548",
     "url": "/dictionary/static/js/2.d42b6f0b.chunk.js"
   },
   {
-    "revision": "7f913f7d80bac4f8fb22",
-    "url": "/dictionary/static/js/main.b2bf2a80.chunk.js"
+    "revision": "5dc6acc4edd0bd369aca",
+    "url": "/dictionary/static/js/main.f021d265.chunk.js"
   },
   {
     "revision": "67d3f92be30e1c42b517",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dictionary/precache-manifest.4710e574feecdf0488a0ff5201431e9f.js"
+  "/dictionary/precache-manifest.552cfb85c66de434e98286114a164dc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dictionary/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
